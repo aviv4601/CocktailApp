1f368eaa62adb2797f53f6c2bcd945a1e51f034d
aviv4601 <amrusiaviv@gmail.com>
2023-05-12 21:32:36 +0300

    change  App

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Script from "next/script";
 import Logo from "../../assets/logo.svg";
 import CocktailList from "./components/CocktailList";
 import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div>
+      <Script src="https://third-party-script.js"></Script>
       <div className="mt-[-70px]">
         <img src={Logo.src}></img>
       </div>
